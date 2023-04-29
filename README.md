37bbbf56c86a9239077dc244cf17a850a823502c
Thomas Su <deng5765@gmail.com>
2023-04-29 21:36:36 +0800

    FTP integration for Craft CMS

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# craft-ftp
-FTP integration for Craft CMS, FTP volume for Craft CMS filesystem
+<p align="center"><img src="./src/icon.svg" width="100" height="100" alt="FTP integration for Craft CMS"></p>
+
+<h1 align="center">FTP integration for Craft CMS</h1>
+
+This plugin provides a FTP integration for [Craft CMS](https://craftcms.com/), with this plugin we can create a FTP volume type for Craft CMS.
+
+## Requirements
+
+ - PHP 8.0.2 or later
+ - Craft CMS 4.0 or later
+
+## Installation
+
+We can install this plugin with Composer.
+Open your terminal and run the following commands:
+
+```bash
+# go to the project directory
+cd /path/to/my-project
+
+# tell Composer to load the plugin
+composer require xinningsu/craft-ftp
+
+# tell Craft to install the plugin
+./craft plugin/install craft-ftp
+```
+
+## Setup
+
+To create a new FTP filesystem to use with your volumes, 
+
+- Login admin, visit **Settings** → **Filesystems**,
+- Press **New filesystem**. 
+- Select “FTP” for the **Filesystem Type** 
+- Setting and configure as needed.
---
+++ src/Fs.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sulao\CraftFtp;
+
+use Craft;
+use craft\behaviors\EnvAttributeParserBehavior;
+use craft\flysystem\base\FlysystemFs;
+use craft\helpers\Assets;
+use League\Flysystem\Config;
+use League\Flysystem\FilesystemAdapter;
+use League\Flysystem\Ftp\FtpAdapter;
+use League\Flysystem\Ftp\FtpConnectionOptions;
+
+class Fs extends FlysystemFs
+{
+    public string  $host = '';
+    public int     $port = 21;
+    public string  $username = '';
+    public string  $password = '';
+    public string  $root = '';
+
+    public static function displayName(): string
+    {
+        return 'FTP';
+    }
+
+    public function behaviors(): array
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['parser'] = [
+            'class'      => EnvAttributeParserBehavior::class,
+            'attributes' => ['host', 'port', 'username', 'password', 'root'],
+        ];
+
+        return $behaviors;
+    }
+
+    public function getSettingsHtml(): ?string
+    {
+        return Craft::$app->getView()->renderTemplate('craft-ftp/fsSettings', [
+            'fs'      => $this,
+            'periods' => array_merge(['' => ''], Assets::periodList()),
+        ]);
+    }
+
+    protected function defineRules(): array
+    {
+        return array_merge(parent::defineRules(), [
+            [['host', 'username', 'password'], 'required'],
+        ]);
+    }
+
+    protected function createAdapter(): FilesystemAdapter
+    {
+        return new FtpAdapter(
+            FtpConnectionOptions::fromArray([
+                'host'     => $this->host,
+                'port'     => $this->port,
+                'username' => $this->username,
+                'password' => $this->password,
+                'root'     => $this->root,
+            ])
+        );
+    }
+
+    protected function addFileMetadataToConfig(array $config): array
+    {
+        return array_merge(
+            parent::addFileMetadataToConfig($config),
+            [Config::OPTION_DIRECTORY_VISIBILITY => $this->visibility()]
+        );
+    }
+
+    protected function invalidateCdnPath(string $path): bool
+    {
+        return true;
+    }
+}
